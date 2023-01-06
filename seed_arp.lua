7f9f42a4010dc5893d5902fd955900fcc9410042
Roy Klein <roeiklein@gmail.com>
2023-01-06 21:33:07 +0100

    add the rest of the sequencer and fix some bugs

--- seed_arp.lua
+++ seed_arp.lua
@@ -7,9 +7,14 @@ randomMapInitiated = false
 randomMap = {}
 randomMapIndex = 1
 melody = {}
+pattern = {}
 melodyIndex = 1
 melodyLength = 0
+arpLaunched = false
 time = Knob("Time", 4, 1, 8, true)
+time.changed = function(self) 
+    arpLaunched = false
+end
 seed = Knob{"Seed", 10, 1, 10, true}
 seed.changed = function(self) 
     resetSeed()
@@ -18,6 +23,7 @@ maxMelodyLength = Knob{"Melody_Length", 4, 2, 8, true, displayName = "Melody L."
 maxMelodyLength.changed = function(self) 
     resetSeed()
 end
+chance = Knob{"OffChance", 10, 1, 10, true}
 
 b1 = OnOffButton("b1", true)
 b1.backgroundColourOff = "darkgrey"
@@ -47,6 +53,34 @@ b4.textColourOff = "white"
 b4.textColourOn = "white"
 b4.bounds = {545,10,10,10}
 
+b5 = OnOffButton("b5", true)
+b5.backgroundColourOff = "darkgrey"
+b5.backgroundColourOn = "darkred"
+b5.textColourOff = "white"
+b5.textColourOn = "white"
+b5.bounds = {500,25,10,10}
+
+b6 = OnOffButton("b6", true)
+b6.backgroundColourOff = "darkgrey"
+b6.backgroundColourOn = "darkred"
+b6.textColourOff = "white"
+b6.textColourOn = "white"
+b6.bounds = {515,25,10,10}
+
+b7 = OnOffButton("b7", true)
+b7.backgroundColourOff = "darkgrey"
+b7.backgroundColourOn = "darkred"
+b7.textColourOff = "white"
+b7.textColourOn = "white"
+b7.bounds = {530,25,10,10}
+
+b8 = OnOffButton("b8", true)
+b8.backgroundColourOff = "darkgrey"
+b8.backgroundColourOn = "darkred"
+b8.textColourOff = "white"
+b8.textColourOn = "white"
+b8.bounds = {545,25,10,10}
+
 isEventPlaying = {}
 
 function resetSeed() 
@@ -55,13 +89,16 @@ function resetSeed()
     end
     melodyLength = 0
     melody = {}
+    pattern = {}
     randomMapIndex = 1;
     print("--generating melody")
     while melodyLength<maxMelodyLength.value do
         math.randomseed(seed.value, 1)
-        noteToPlay = getRandom()
+        local noteToPlay = getRandom()
         local skip = getRandom()
+        table.insert(pattern, skip)
         table.insert(melody, noteToPlay)
+        print(noteToPlay,skip)
         melodyLength = melodyLength +1
     end
     print("--done",melodyLength)
@@ -91,19 +128,20 @@ function tableLength(T)
   return count
 end
 
-local arpLaunched
 function arp()
-    while true do
+    while arpLaunched do
 
         local len = tableLength(isEventPlaying)
         if (len == 0) then break end
-        local noteToPlay = melody[melodyIndex]
         
+        local noteToPlay = melody[(melodyIndex % melodyLength) + 1]
+        local notePattern = pattern[(melodyIndex  % melodyLength) + 1]
         local currentIndex = melodyIndex
         melodyIndex = melodyIndex+1
         if (melodyIndex>melodyLength) then
             melodyIndex = 1 
         end
+        print(currentIndex,notePattern)
         local notes = {};
         for k, e in pairs(isEventPlaying) do
             table.insert(notes, e) 
@@ -111,15 +149,26 @@ function arp()
         table.sort(notes, function(a,b) return a.note<b.note end)
          
         local beat = 1/time.value;
-        print(melodyIndex, currentIndex)
-        if (melodyIndex==1 and b1.value==false) then
+        local maybeSkip = false
+        if (currentIndex==1 and b1.value==false) then
+            maybeSkip = true
+        elseif (currentIndex==2 and b2.value==false) then
+            maybeSkip = true
+        elseif (currentIndex==3 and b3.value==false) then
+            maybeSkip = true
+        elseif (currentIndex==4 and b4.value==false) then            
+            maybeSkip = true
+        elseif (currentIndex==5 and b5.value==false) then
+            maybeSkip = true
+        elseif (currentIndex==6 and b6.value==false) then
+            maybeSkip = true
+        elseif (currentIndex==7 and b7.value==false) then
+            maybeSkip = true
+        elseif (currentIndex==8 and b8.value==false) then            
+            maybeSkip = true
+        end
+        if (maybeSkip==true and notePattern<=chance.value) then
             waitBeat(beat)
-        elseif (melodyIndex==2 and b2.value==false) then
-            waitBeat(beat);
-        elseif (melodyIndex==3 and b3.value==false) then
-            waitBeat(beat);
-        elseif (melodyIndex==4 and b4.value==false) then            
-            waitBeat(beat);
         else
 	        local i = 1
             local note = (noteToPlay % len) + 1
@@ -143,8 +192,8 @@ end
 local idsPlaying
 function onNote(e)
     isEventPlaying[e.id] = e
-    melodyIndex = 1
     if not arpLaunched then
+        melodyIndex = 1
         resetSeed()
         arpLaunched = true
         run(arp)
