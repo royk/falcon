2e3731f180be4796583949904be920112f9e2b49
Roy Klein <roeiklein@gmail.com>
2023-01-08 13:00:06 +0100

    add replayer via json

--- psuedorandom_arp.lua
+++ psuedorandom_arp.lua
@@ -4,6 +4,11 @@
 --! Date : 26/01/2023
 --------------------------------------------------------------------------------
 randomMapInitiated = false
+recorder = {}
+recording = false
+playing = false
+playLaunched = false
+playIndex = 0
 randomMelody = {}
 randomGate = {}
 randomMapIndex = 1
@@ -32,8 +37,39 @@ seed.changed = function(self)
     resetSeed()
 end
 
---rec = OnOffButton{"Rec", false}
-
+rec = OnOffButton{"Rec", false}
+rec.backgroundColourOn = "darkred"
+rec.changed = function(self)
+    if (self.value==true) then
+        recorder = {}
+        recording = true
+        print("Recording")
+    else
+        saveData(recorder, 'd:/record.json')
+        print(recorder)
+    end
+end
+rec.bounds = {0,60,50,20}
+load = Button("Load")
+load.changed = function() 
+    browseForFile('open', 'Load recording', '', '*.json', function(task)
+        loadData(task.result, function (data)
+            recorder = data
+        end)
+    end)
+end
+load.bounds = {60,60,50,20}
+play = OnOffButton{"Replay", false}
+play.changed = function(self)
+    if (self.value==true) then 
+        playing = true
+        playIndex = 1
+        arpLaunched = false
+    else
+        playing = false
+    end
+end
+play.bounds = {120,60,50,20}
 sequencer = {}
 for i = 1,8,1 do
     sequencer[i] = OnOffButton("sequencer"..tostring(i), false)
@@ -41,10 +77,10 @@ for i = 1,8,1 do
     sequencer[i].backgroundColourOn = "darkred"
     sequencer[i].textColourOff = "white"
     sequencer[i].textColourOn = "white"
-    local y = 10
+    local y = 15
     local x = i-1
     if i>4 then
-        y = 25
+        y = y+15
         x = x-4
     end
     sequencer[i].bounds = {600+15*x,y,10,10}
@@ -98,12 +134,34 @@ end
 function tableLength(T)
   local count = 0
   for _ in pairs(T) do count = count + 1 end
-  return count
+  return count 
+end
+
+function replay()
+    local len = tableLength(recorder)
+    if (len==0) then
+        return
+    end
+    while playing do
+        local e = recorder[playIndex]
+        print(playIndex,' ',len)
+        if (playIndex>=len) then
+            playIndex = 0
+        end
+        if (type(e)=="number") then
+            waitBeat(e)
+        else
+            playNote(e.note, e.velocity, 10 , e.layer, e.channel, e.input, e.vol, e.pan, e.tune, e.slice)
+        end        
+
+        playIndex = playIndex+1
+    end
+    playing = false
 end
 
 function arp()
+    arpLaunched = false
     while arpLaunched do
-
         local len = tableLength(isEventPlaying)
         if (len == 0) then break end
         
@@ -129,12 +187,19 @@ function arp()
             end 
         end
         if (maybeSkip==true and notePattern<=chance.value) then
+            if (recording) then
+                table.insert(recorder, beat);
+            end
             waitBeat(beat)
         else
 	        local i = 1
             local note = (noteToPlay % len) + 1
 	        for k, e in pairs(notes) do
 	            if (i == note) then
+                    if (recording) then
+                        table.insert(recorder, e);
+                        table.insert(recorder, beat);
+                    end
 	                playNote(e.note, e.velocity, 10 , e.layer, e.channel, e.input, e.vol, e.pan, e.tune, e.slice)
 	                waitBeat(beat)
 	                break
@@ -153,7 +218,13 @@ end
 local idsPlaying
 function onNote(e)
     isEventPlaying[e.id] = e
-    if not arpLaunched then
+    if playing then
+        if not playLaunched then
+            playIndex = 1
+            run(replay)
+            playLaunched = true
+        end
+    elseif not arpLaunched then
         melodyIndex = 1
         resetSeed()
         arpLaunched = true
