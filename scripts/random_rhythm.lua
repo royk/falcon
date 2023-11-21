e3a82270203adc70239428be872195b1330af0c5
Roy Klein <roeiklein@gmail.com>
2023-11-21 15:42:36 +0100

    make all patterns and their params persist

--- scripts/random_rhythm.lua
+++ scripts/random_rhythm.lua
@@ -8,9 +8,6 @@
 patternMaxLength = 16
 maxPatterns = 8;
 pattern = {}
-patterns = {}
-patternLengths = {}
-patternBeats = {}
 patternPosition = 0
 patternShiftAmount = 0
 customUIXPosition = 490
@@ -19,41 +16,43 @@ randomizeButton.changed = function(self)
   randomize(getTime())
 end
 randomizeButton.bounds = {customUIXPosition+120, 5, 110, 20}
-saveButton = Button("Save", false)
-saveButton.changed = function(self)
-  patternShiftAmount = 0
-  patternShift.value = 0
-  patterns[patternSelector.value] = cloneArray(pattern)
-  patternLengths[patternSelector.value] = patternLengthSelector.value
-  patternBeats[patternSelector.value] = beatsSelector.value
-end
-saveButton.bounds = {customUIXPosition+120, 30, 110, 20}
 
 patternSelector = Knob{"Pattern", 1, 1, 8, true}
 patternSelector.changed = function(self) 
-  if patternShiftAmount > 0 then
-    shiftLeft(patternShiftAmount)
-  elseif patternShiftAmount < 0 then
-    shiftRight(patternShiftAmount)
-  end
   patternShiftAmount = 0
   patternShift.value = 0
-  pattern = patterns[patternSelector.value]
-  patternLengthSelector.value = patternLengths[patternSelector.value]
-  beatsSelector.value = patternBeats[patternSelector.value]
-  updatePatternDisplay()
+  for i = 1,patternMaxLength,1 do
+    index = getSequencerStepIndex(i)
+    pattern[i] = sequencer[index].value and 1 or 0
+  end
+  redrawControls()
+  drawSequencer()
+end
+beatsSelectors = {}
+for i= 1, maxPatterns,1  do
+  beatsSelectors[i] = Knob{"Beats"..tostring(i), 4, 1, 8, true, displayName="Beats"}
+  beatsSelectors[i].changed = function(self)
+    updatePatternDisplay()
+  end
+  beatsSelectors[i].width = i==1 and 110 or 0
+  beatsSelectors[i].visible = false
+end
+patternLengthSelectors = {}
+for i= 1, maxPatterns,1  do
+  patternLengthSelectors[i] = Knob{"Pattern_Length"..tostring(i), maxPatterns, 1, patternMaxLength, true}
+  patternLengthSelectors[i].changed = function(self)
+    updatePatternDisplay()
+  end
+  patternLengthSelectors[i].width = i==1 and 110 or 0
+  patternLengthSelectors[i].visible = false
 end
 
-beatsSelector = Knob{"Beats", 4, 1, 8, true}
-
-patternLengthSelector = Knob{"Pattern_Length", maxPatterns, 1, patternMaxLength, true}
-patternLengthSelector.changed = function(self)
-  updatePatternDisplay()
-end
 
 patternShift = Knob{"Pattern_Shift", 0, patternMaxLength/-2, patternMaxLength/2-1, true}
 patternShift.changed = function(self)
-  -- shift sequencer positions based on value of patternShift
+  if patternShiftAmount==patternShift.value then 
+    return
+  end
   if patternShiftAmount > patternShift.value then
     shiftLeft(patternShiftAmount - patternShift.value)
   else
@@ -63,12 +62,25 @@ patternShift.changed = function(self)
   updatePatternDisplay()
 end
 
-function cloneArray(arr)
-  local new = {}
-  for i = 1, patternMaxLength do
-    new[i] = arr[i]
+function redrawControls()
+  for i= 1, maxPatterns,1  do
+    if i==patternSelector.value then
+      patternLengthSelectors[i].visible = true
+      patternLengthSelectors[i].width = 110
+      patternLengthSelectors[i].x = 245
+      patternLengthSelectors[i].y = 5
+      beatsSelectors[i].visible = true
+      beatsSelectors[i].width = 110
+      beatsSelectors[i].x = 135
+      beatsSelectors[i].y = 5
+    else
+      patternLengthSelectors[i].visible = false
+      patternLengthSelectors[i].width = 0
+      beatsSelectors[i].visible = false
+      beatsSelectors[i].width = 0
+      
+    end
   end
-  return new
 end
 
 function shiftLeft(amount)
@@ -91,37 +103,48 @@ function shiftRight(amount)
   end
 end
 
+function getSequencerStepIndex(i)
+  return (patternSelector.value-1)*patternMaxLength+i;
+end
+
 function updatePatternDisplay() 
   for i = 1,patternMaxLength,1 do
-      sequencer[i].enabled = i<=patternLengthSelector.value
-      sequencer[i].value = pattern[i]==1
+    index = getSequencerStepIndex(i)
+    sequencer[index].enabled = i<=patternLengthSelectors[patternSelector.value].value
+    sequencer[index].value = pattern[i]==1
   end
 end
 
 sequencer = {}
-for i = 1,patternMaxLength,1 do
-    sequencer[i] = OnOffButton("sequencer"..tostring(i), false)
-    sequencer[i].backgroundColourOff = "darkgrey"
-    sequencer[i].backgroundColourOn = "darkred"
-    sequencer[i].textColourOff = "white"
-    sequencer[i].textColourOn = "white"
-    local row = math.floor((i-1)/8)+1
-    local y = (20*row)-10
-    local x = (i-1)%8
-    sequencer[i].bounds = {customUIXPosition+15*x,y,10,10}
-    sequencer[i].enabled = i<=8
-    sequencer[i].changed = function(self)
-      pattern[i] = sequencer[i].value and 1 or 0
-    end
+for j= 1, maxPatterns,1  do
+  for i = 1,patternMaxLength,1 do
+      index = (j-1)*patternMaxLength+i ;
+      sequencer[index] = OnOffButton("sequencer"..tostring(index), false)
+      sequencer[index].backgroundColourOff = "darkgrey"
+      sequencer[index].backgroundColourOn = "darkred"
+      sequencer[index].textColourOff = "white"
+      sequencer[index].textColourOn = "white"
+      local row = math.floor((i-1)/8)+1
+      local y = (20*row)-10
+      local x = (i-1)%8
+      sequencer[index].bounds = {customUIXPosition+15*x,y,10,10}
+      sequencer[index].enabled = i<=8
+      sequencer[index].changed = function(self)
+        pattern[i] = sequencer[getSequencerStepIndex(i)].value and 1 or 0
+      end
+      sequencer[index].visible = j==1
+  end
 end
 
-for i = 1, maxPatterns, 1 do 
-  patterns[i] = {}
-  for j = 1, patternMaxLength, 1 do
-    patterns[i][j] = false
+redrawControls()
+
+function drawSequencer()
+  for j= 1, maxPatterns,1  do
+    for i = 1,patternMaxLength,1 do
+      index = (j-1)*patternMaxLength+i ;
+      sequencer[index].visible = j==patternSelector.value
+    end
   end
-  patternLengths[i] = 8
-  patternBeats[i] = 4
 end
 
 function randomize(seed) 
@@ -130,20 +153,21 @@ function randomize(seed)
   for i = 1, patternMaxLength, 1 do
     local val = math.random(0,1)
     table.insert(pattern, val)
-    sequencer[i].value = val==1
+    sequencer[getSequencerStepIndex(i)].value = val==1
   end
 end
 
 function onNote(e)
   updatePatternDisplay()
-  local beatPos = math.floor((getRunningBeatTime() * beatsSelector.value ) % patternMaxLength)
-  patternPosition = beatPos  % patternLengthSelector.value + 1
-  sequencer[patternPosition].enabled = false
-  if sequencer[patternPosition].value == true then
+  local beatPos = math.floor((getRunningBeatTime() * beatsSelectors[patternSelector.value].value ) % patternMaxLength)
+  patternPosition = beatPos  % patternLengthSelectors[patternSelector.value].value + 1
+  index = getSequencerStepIndex(patternPosition)
+  sequencer[index].enabled = false
+  if sequencer[index].value == true then
     playNote(e.note, e.velocity, e.duration , e.layer, e.channel, e.input, e.vol, e.pan, e.tune, e.slice)
   end
   patternPosition = patternPosition + 1 
-  if patternPosition > patternLengthSelector.value then 
+  if patternPosition > patternLengthSelectors[patternSelector.value].value then 
     patternPosition = 1
   end
   
